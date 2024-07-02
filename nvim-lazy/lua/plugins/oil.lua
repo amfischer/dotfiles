7522f6529f9ef2b3c1a73bc61a6d0255c0f2d6e5
Aaron F <amfischer21@gmail.com>
2024-07-02 08:11:48 -0500

    updates & refactors

--- nvim-lazy/lua/plugins/oil.lua
+++ nvim-lazy/lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 return {
-    "stevearc/oil.nvim",
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    {
+        "stevearc/oil.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("oil").setup()
+            vim.keymap.set("n", "<A-o>", ":Oil<CR>")
+        end,
+    },
 }
--- nvim-lazy/lua/plugins/treesitter.lua
+++ nvim-lazy/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
     { -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        dependencies = { 
-            'nvim-treesitter/nvim-treesitter-textobjects',
-            'JoosepAlviste/nvim-ts-context-commentstring',
-            'windwp/nvim-ts-autotag'
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "JoosepAlviste/nvim-ts-context-commentstring",
+            "windwp/nvim-ts-autotag",
         },
         opts = {
-            ensure_installed = 'all',
+            ensure_installed = "all",
             -- Autoinstall languages that are not installed
             auto_install = true,
             highlight = {
@@ -16,23 +16,23 @@ return {
                 -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
                 --  If you are experiencing weird indenting issues, add the language to
                 --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-                additional_vim_regex_highlighting = { 'ruby' },
+                additional_vim_regex_highlighting = { "ruby" },
             },
             indent = {
                 enable = true,
-                disable = { 'ruby' }
+                disable = { "ruby" },
             },
             textobjects = {
                 select = {
                     enable = true,
                     lookahead = true,
                     keymaps = {
-                        ['if'] = '@function.inner',
-                        ['af'] = '@function.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['aa'] = '@parameter.outer',
+                        ["if"] = "@function.inner",
+                        ["af"] = "@function.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["aa"] = "@parameter.outer",
                     },
-                }
+                },
             },
             autotag = {
                 enable = true,
@@ -42,9 +42,9 @@ return {
             -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
             -- Prefer git instead of curl in order to improve connectivity in some environments
-            require('nvim-treesitter.install').prefer_git = true
+            require("nvim-treesitter.install").prefer_git = true
             ---@diagnostic disable-next-line: missing-fields
-            require('nvim-treesitter.configs').setup(opts)
+            require("nvim-treesitter.configs").setup(opts)
 
             -- There are additional nvim-treesitter modules that you can use to interact
             -- with nvim-treesitter. You should go explore a few and see what interests you:
