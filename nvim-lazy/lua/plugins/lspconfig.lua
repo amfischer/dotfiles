d2bc207f4c3d5503600f4046f30417ea554a04d7
Aaron F <amfischer21@gmail.com>
2025-06-07 20:27:40 -0500

    fix vue lsp for mason 2

--- nvim-lazy/lua/plugins/lspconfig.lua
+++ nvim-lazy/lua/plugins/lspconfig.lua
@@ -112,7 +112,7 @@ return {
                     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
                 },
 
-                volar = {
+                vue_ls = {
                     -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
                     -- init_options = {
                     --     vue = {
