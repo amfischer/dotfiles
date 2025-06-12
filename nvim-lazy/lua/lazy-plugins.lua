7cf3dbba889a0d4576b57bf25ea7ae8ee8b023e4
Aaron F <amfischer21@gmail.com>
2025-06-11 21:24:47 -0500

    add neotest and leap

--- nvim-lazy/lua/lazy-plugins.lua
+++ nvim-lazy/lua/lazy-plugins.lua
@@ -113,6 +113,13 @@ require("lazy").setup({
     },
 
     {
+        "ggandor/leap.nvim",
+        config = function()
+            require("leap").set_default_mappings()
+        end,
+    },
+
+    {
         "dense-analysis/ale",
         config = function()
             vim.g.ale_fixers = {
@@ -163,7 +170,43 @@ require("lazy").setup({
             vim.keymap.set("n", "<A-0>", ":BufferLineCycleNext<CR>")
             vim.keymap.set("n", "<A-(>", ":BufferLineMovePrev<CR>")
             vim.keymap.set("n", "<A-)>", ":BufferLineMoveNext<CR>")
-        end
+        end,
+    },
+
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "V13Axel/neotest-pest",
+        },
+        config = function()
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-pest")({
+                        sail_enabled = function()
+                            return false
+                        end,
+                        sail_executable = { "docker", "exec", "[coid-app-laravel.test-1]" },
+                        results_path = function()
+                            return "storage/app/" .. os.date("junit-%Y%m%d-%H%M%S")
+                        end,
+                        -- pest_cmd = "vendor/bin/pest"
+                    }),
+                },
+                -- floating = {
+                --     border = "rounded",
+                --     max_height = 0.6,
+                --     max_width = 0.6,
+                --     options = {},
+                -- },
+            })
+
+            vim.keymap.set("n", "<leader>tn", ":Neotest run<CR>")
+            vim.keymap.set("n", "<F3>", ":Neotest output-panel<CR>")
+        end,
     },
 
     require("plugins/vim-tmux-navigator"),
