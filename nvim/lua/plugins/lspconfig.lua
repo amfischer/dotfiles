ca025377c9a22ddf20a70d5518ddfa8a8dccc566
Aaron F <amfischer21@gmail.com>
2026-03-11 10:13:44 -0500

    fix and update lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,70 +4,37 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        -- Automatically install LSPs and related tools to stdpath for Neovim
-        { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        "b0o/schemastore.nvim",
-
-        -- Useful status updates for LSP.
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-        -- { 'j-hui/fidget.nvim', opts = {} },
-
-        { "folke/lazydev.nvim", opts = {} },
+        "mason-org/mason.nvim",
+        "mason-org/mason-lspconfig.nvim",
+        "b0o/SchemaStore.nvim",
     },
     config = function()
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-t>.
-        vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions)
+        -- Setup Mason to automatically install LSP servers
+        require("mason").setup()
+        require("mason-lspconfig").setup { automatic_installation = true }
 
-        -- Find references for the word under your cursor.
-        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references)
+        local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        vim.keymap.set("n", "gI", require("telescope.builtin").lsp_implementations)
+        -- PHP
+        vim.lsp.enable "intelephense"
+        vim.lsp.config("intelephense", {
+            capabilities = capabilities,
+            init_options = {
+                licenceKey = "/home/aaron/intelephense/licence.txt",
+            },
+            settings = {
+                intelephense = {
+                    files = {
+                        exclude = { "**/vendor/_laravel_ide" },
+                    },
+                },
+            },
+        })
 
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        vim.keymap.set("n", "<leader>D", require("telescope.builtin").lsp_type_definitions)
+        -- Vue, JavaScript, TypeScript
 
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        vim.keymap.set("n", "<leader>s", require("telescope.builtin").lsp_document_symbols)
+        local vue_language_server_path = vim.fn.expand "$MASON/packages" .. "/vue-language-server" .. "/node_modules/@vue/language-server"
 
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols)
-
-        -- Rename the variable under your cursor.
-        --  Most Language Servers support renaming across files, etc.
-        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
-
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
-        vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
-
-        -- Opens a popup that displays documentation about the word under your cursor
-        --  See `:help K` for why this keymap.
-        vim.keymap.set("n", "K", vim.lsp.buf.hover)
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-
-        -- LSP servers and clients are able to communicate to each other what features they support.
-        --  By default, Neovim doesn't support everything that is in the LSP specification.
-        --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-        --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-        local vue_language_server_path = vim.fn.expand("$MASON/packages/")
-            .. "/vue-language-server"
-            .. "/node_modules/@vue/language-server"
         local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
 
         local vue_plugin = {
@@ -99,102 +66,98 @@ return {
             filetypes = tsserver_filetypes,
         }
 
-        local vue_ls_config = {}
+        local vue_ls_config = {
+            capabilities = capabilities,
+        }
 
         vim.lsp.config("vtsls", vtsls_config)
         vim.lsp.config("vue_ls", vue_ls_config)
         vim.lsp.config("ts_ls", ts_ls_config)
-        vim.lsp.enable({ "vtsls", "vue_ls" }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
+        vim.lsp.enable { "vtsls", "vue_ls" }
 
-        vim.lsp.config("intelephense", {
-            -- init_options = {
-            --     licenseKey = "/home/aaron/intelephense/licence.txt",
-            -- },
+        -- Tailwind CSS
+        vim.lsp.enable "tailwindcss"
+        vim.lsp.config("tailwindcss", {
+            capabilities = capabilities,
+            filetypes = { "html", "vue", "typescript" },
             settings = {
-                intelephense = {
+                tailwindCSS = {
                     files = {
-                        exclude = { "**/vendor/_laravel_ide" },
+                        exclude = { "**/vendor/**" },
                     },
                 },
             },
         })
 
-        -- Enable the following language servers
-        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-        --
-        --  Add any additional override configuration in the following tables. Available keys are:
-        --  - cmd (table): Override the default command used to start the server
-        --  - filetypes (table): Override the default list of associated filetypes for the server
-        --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-        --  - settings (table): Override the default settings passed when initializing the server.
-        --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-        local servers = {
-
-            lua_ls = {
-                settings = {
-                    Lua = {
-                        completion = {
-                            callSnippet = "Replace",
-                        },
-                        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                        diagnostics = { disable = { "missing-fields" } },
-                    },
+        -- JSON
+        vim.lsp.enable "jsonls"
+        vim.lsp.config("jsonls", {
+            capabilities = capabilities,
+            settings = {
+                json = {
+                    schemas = require("schemastore").json.schemas(),
+                    validate = { enable = true },
                 },
             },
+        })
 
-            intelephense = {
-                settings = {
-                    -- init_options = {
-                    --     licenseKey = "/home/aaron/intelephense/licence.txt",
-                    -- },
-                },
-            },
+        -- Lua
+        vim.lsp.enable "lua_ls"
+        vim.lsp.config("lua_ls", {
+            capabilities = capabilities,
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if path ~= vim.fn.stdpath "config" and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc")) then return end
+                end
 
-            tailwindcss = {
-                filetypes = { "html", "vue", "typescript" },
-                settings = {
-                    tailwindCSS = {
-                        files = {
-                            exclude = { "**/vendor/**/stubs/**" },
+                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most
+                        -- likely LuaJIT in the case of Neovim)
+                        version = "LuaJIT",
+                        -- Tell the language server how to find Lua modules same way as Neovim
+                        -- (see `:h lua-module-load`)
+                        path = {
+                            "lua/?.lua",
+                            "lua/?/init.lua",
                         },
                     },
-                },
-            },
-
-            jsonls = {
-                settings = {
-                    json = {
-                        schemas = require("schemastore").json.schemas(),
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            -- Depending on the usage, you might want to add additional paths
+                            -- here.
+                            -- '${3rd}/luv/library'
+                            -- '${3rd}/busted/library'
+                        },
+                        -- Or pull in all of 'runtimepath'.
+                        -- NOTE: this is a lot slower and will cause issues when working on
+                        -- your own configuration.
+                        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+                        -- library = {
+                        --   vim.api.nvim_get_runtime_file('', true),
+                        -- }
                     },
-                },
-            },
-        }
-
-        require("mason").setup()
-
-        -- You can add other tools here that you want Mason to install
-        -- for you, so that they are available from within Neovim.
-        local ensure_installed = vim.tbl_keys(servers or {})
-        vim.list_extend(ensure_installed, {
-            "stylua", -- Used to format Lua code
-            "vue-language-server",
-            "vtsls",
-        })
-
-        require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-        require("mason-lspconfig").setup({
-            handlers = {
-                function(server_name)
-                    local server = servers[server_name] or {}
-                    -- This handles overriding only values explicitly passed
-                    -- by the server configuration above. Useful when disabling
-                    -- certain features of an LSP (for example, turning off formatting for tsserver)
-                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-                    require("lspconfig")[server_name].setup(server)
-                end,
+                })
+            end,
+            settings = {
+                Lua = {},
             },
         })
+
+        -- Keymaps
+        vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions)
+        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references)
+        vim.keymap.set("n", "gI", require("telescope.builtin").lsp_implementations)
+        vim.keymap.set("n", "<leader>D", require("telescope.builtin").lsp_type_definitions)
+        vim.keymap.set("n", "<leader>s", require("telescope.builtin").lsp_document_symbols)
+        vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
+        vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover)
     end,
 }
 -- vim: ts=2 sts=2 sw=2 et
